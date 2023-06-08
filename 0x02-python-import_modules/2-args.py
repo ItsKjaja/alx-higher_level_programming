728f439339d6c7bfd34ab23e085c0fc685ecc493
ItsKjaja <contact.kjaj@gmail.com>
2023-06-08 15:55:12 -0700

    argum

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,9 +3,9 @@ from sys import argv
 if __name__ == "__main__":
     argc = len(argv) - 1
     if argc == 0:
-        print("{:d} arguments:".format(argc))
+        print("{:d} arguments.".format(argc))
     elif argc == 1:
-        print("{:d} arguments:".format(argc))
+        print("{:d} argument:".format(argc))
     else:
         print("{:d} arguments:".format(argc))
     for i in range(0, len(argv)):
