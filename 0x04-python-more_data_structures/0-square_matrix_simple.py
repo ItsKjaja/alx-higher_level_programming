b33784dd7d1524bd244e6d78863a275b1cc5c233
ItsKjaja <contact.kjaj@gmail.com>
2023-06-14 08:38:43 -0700

    task 0 & README

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+
+def square_matrix_simple(matrix=[]):
+    result = []
+    for row in matrix:
+        new_row = list(map(lambda x: x * x, row))
+        result.append(new_row)
+    return result
