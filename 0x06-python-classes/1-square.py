cc04913fa5497d9b4f9a8a2680f36bfca0e696b1
ItsKjaja <contact.kjaj@gmail.com>
2023-06-27 06:16:01 -0700

    task1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
-class Square
-  """class Square that defines a square"""
 
-  def __init__(self, size):
-      """ Class Constructor"""
-      self.___size = size
+"""Define a class square"""
+
+
+class Square:
+    """Represent a square"""
+
+    def __init__(self, size):
+        """Initialize a new Square.
+        Args:
+            size (int): The size of the new square.
+        """
+        self.__size = size
