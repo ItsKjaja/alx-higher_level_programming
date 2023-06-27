d70e313d04a0d6ab6116ad0df96f52692a98c857
ItsKjaja <contact.kjaj@gmail.com>
2023-06-27 15:03:49 -0700

    task3

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -15,7 +15,8 @@ class Square:
                TypeError: size is not integer.
                ValueError: if size less than 0.
        """
-       if not isinstance(size, int):
+    
+      if not isinstance(size, int):
            raise TypeError("size must be an integer")
        if size < 0:
            raise ValueError("size must be >= 0")
