5de514c3dd0d1c0f702e4ba69aa17680a09de7b4
ItsKjaja <contact.kjaj@gmail.com>
2023-07-10 08:55:45 -0700

    list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Mylist module"""
+
+
+
+class MyList(list):
+    """Mylist class"""
+
+    def print_sorted(self):
+        """print sorted list"""
+        print(sorted(self))
