911585ebeb8704390a66abc2523644752867206a
ItsKjaja <contact.kjaj@gmail.com>
2023-07-11 06:31:43 -0700

    fixing error

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+"""defines a text file-reading function"""
 
 
-def read_file(filename=""):
+def read_file(filename=''):
     """function that reads a text file (UTF8) and prints it to stdout"""
 
     with open(filename, encoding="utf-8") as myfile:
