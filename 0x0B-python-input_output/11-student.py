65a32006d1f3b0e7439048fcbdf6a543e3b65fb7
ItsKjaja <contact.kjaj@gmail.com>
2023-07-11 10:04:03 -0700

    student

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -16,5 +16,13 @@ class Student:
         if type(attrs) == list:
             for attr in attrs:
                 if type(attr) != str:
-                    return {k: getattr(self, k) for k in attrs if hasattr(self, k);=
+                    return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
         return self.__dict__
+
+    def reload_from_json(self, json):
+        """ from json to student class
+            Args:
+                json: json dict
+        """
+        for key, value in json.items():
+            setattr(self, key, value)
