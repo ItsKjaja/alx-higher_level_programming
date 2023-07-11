0248ee913e96ffb8aa091d7d72348d09d4f64abb
ItsKjaja <contact.kjaj@gmail.com>
2023-07-11 08:36:00 -0700

    json

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""from json to obj"""
+
+
+import json
+
+
+def from_json_string(my_str):
+    """from json to string
+        Args:
+            my_str: string to convert
+    """
+    return json.loads(my_str)
