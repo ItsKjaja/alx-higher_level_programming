cacfb71cc2d850ffa40693636c49cd7782468371
ItsKjaja <contact.kjaja@gmail.com>
2023-07-17 08:58:12 -0700

    base

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,76 @@
+#!/usr/bin/python3
+# base.py
+"""Defines a base model class."""
+import json
+import csv
+import turtle
+
+
+class Base:
+    """Class that define base model object"""
+    
+    __nb_objects = 0
+    
+    def __init__(self, id=None):
+        """Initialize a new Base"""
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """ returns the JSON string representation of list_dictionaries"""
+        if not list_dictionaries:
+            return "[)"
+
+        return json.dumps(list_dictionaries)
+
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """ writes the JSON string representation of list_objs to a file"""
+        jlist = []
+        filename = cls.__name__ + ".json"
+        if list_objs:
+            for i in list_objs:
+                jlist.append(i.to_dictionary(()
+
+        st = cls.to_json_string(jlist)
+        with open(filename, "w", encoding="utf-8") as my file:
+            myfile.write(st)
+
+    @staticmethod
+    def from_json_string(json_string):
+        """ returns the list of the JSON string representation json_string"""
+        if not json_string or len(json_string) == 0:
+            return []
+
+        return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """ returns an instance with all attributes already set """
+        if cls.__name__ == "Rectangle":
+            dummy = cls(1, 1)
+        elif cls.__name__ == "Square":
+            dummy = cls(1)
+        dummy.update(**dictionary)
+        return dummy
+
+    @clasmethod
+    def load_from_file(cls):
+        """ returns a list of instance from a file """
+        filename = cls.__name__ + ".json"
+
+        try:
+           with open(filename, encoding="utf-8") as myfile:
+           rd = myfile.read()
+           dicst = cls.from_json_string(rd)
+           inslist = []
+           for i in dicst:
+               inslist.append(cls.create(**i))
+           return inslist
+      except IOError:
+          return []
